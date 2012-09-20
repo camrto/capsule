e25be5c181615e332a8efe3cfc90c90b718f6100
camrto <camrto@gmail.com>
2012-09-20 03:01:47 -0500

    commit

---
+++ capsule/WebContent/js/capsules.js
@@ -0,0 +1,12 @@
+//capsule examples by camrto at github dot com is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+
+(function($){$.capsule([
+                        
+	{
+		html:function(){
+			return "<$(tag)>$(content)</$(tag)>";
+		}
+	}
+                      
+
+]);})(jQuery);
